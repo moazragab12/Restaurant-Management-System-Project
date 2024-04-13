664dc71d8b71d4e4294ef480361bae622b1ae958
EsraaAtef2101016 <esraaatef011529@gmail.com>
2024-04-13 18:30:47 +0200

    update Menu ,Item and Main classes

--- projectpart1/src/projectpart1/Menu.java
+++ projectpart1/src/projectpart1/Menu.java
@@ -8,6 +8,88 @@ package projectpart1;
  *
  * @author dell
  */
-public class Menu {
+import java.util.ArrayList;
+
+public class Menu implements Items{
+    protected ArrayList<String>menuitems = new ArrayList<>();
+    protected ArrayList<Double>price_menuitems=new ArrayList<>();
+    protected ArrayList<String>type_menuitems = new ArrayList<>();
+
+    public Menu() {
+    }
+    public Menu(ArrayList<String> type_menuitems,ArrayList<String> menuitems,ArrayList<Double> price_menuitems){
+    this.setMenuitems( menuitems);
+    this.setPrice_menuitems( price_menuitems);
+    this.setType_menuitems(type_menuitems);
+    }
+    
+    
+    // setter and getter menuitems  
+    public ArrayList<String> getMenuitems() {
+        return menuitems;
+    }
+ 
+    private void setMenuitems(ArrayList<String> menuitems) {
+        this.menuitems = menuitems;
+    }
+
+  
+
+    
+//    setter and getter price_menuitems
+    public ArrayList<Double> getPrice_menuitems() {
+        return price_menuitems;
+    }
+    private void setPrice_menuitems(ArrayList<Double> price_menuitems) {
+        this.price_menuitems = price_menuitems;
+    }   
+
+    
+    
+//    setter and getter type_menuitems   
+    public ArrayList<String> getType_menuitems() {
+        return type_menuitems;
+    }
+
+    public void setType_menuitems(ArrayList<String> type_menuitems) {
+        this.type_menuitems = type_menuitems;
+      
+    }
+    
+    
+    
+   
+    @Override
+    public void additem(String type,String item,double price) {
+       int i=type_menuitems.lastIndexOf(type);
+      menuitems.add(i+1,item);    
+    price_menuitems.add(i+1,price);
+    type_menuitems.add(i+1,type);
+    }
+
+    @Override
+    public void removeitem(String item) {
+     int i=menuitems.indexOf(item);
+    menuitems.remove(item);
+    price_menuitems.remove(i);
+    type_menuitems.remove(i);
+    }
+    
+  
+   public void displayMenu(){
+      System.out.println("Menu");
+      System.out.println("Dishs");
+     //  System.out.println("item"+"\t"+"price");
+      for(int i=0 ; i< menuitems.size() ; i++  ){
+          if(type_menuitems.get(i)=="drink"&&type_menuitems.get(i-1)!="drink")
+              System.out.println("Drinks");
+      System.out.println("\t"+menuitems.get(i)+"\t"+ price_menuitems.get(i));
+     }}   
+    
+    @Override
+    public void changeprice(String item, double new_price){
+        price_menuitems.set(menuitems.indexOf(item), new_price);
+    }   
+    
     
 }
--- projectpart1/src/projectpart1/Projectpart1.java
+++ projectpart1/src/projectpart1/Projectpart1.java
@@ -4,6 +4,8 @@
  */
 package projectpart1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dell
@@ -21,6 +23,67 @@ public class Projectpart1 {
         s1.reserve(5);
          s1.change_status(1);
          s1.reserve(5);
+         
+         /////////////////////////////////////////////////////////
+         ///////////////////////////////////////////////////////
+         
+         
+         System.out.println("\n"+"Esraa"+"\n");
+         
+         //     Menu item =new Menu();
+      
+//      item.additem("fish");
+//      item.additem("meat");
+//      item.additem("chicken");
+//      
+//      System.out.println( item.getMenuitems());
+      
+//      item.removeitem("meat");
+//      System.out.println("After removing 'meat': " +item.getMenuitems()); 
+//        
+       ArrayList<String> menuList = new ArrayList<>();
+        menuList.add("fish");
+        menuList.add("meat");
+        menuList.add("coffee");
+//        item.setMenuitems(menuList);
+//         System.out.println( item.getMenuitems());
+         
+         
+         ArrayList<Double> priceList = new ArrayList<>();
+        priceList.add(200.0);
+        priceList.add(300.0);
+        priceList.add(100.0);
+
+ 
+//        item.setPrice_menuitems(priceList);
+//         System.out.println( item.getPrice_menuitems());
+        //changeprice("fish",200000);
+        // item.displayMenu();
+         
+         ArrayList<String> typeList = new ArrayList<>();
+        typeList.add("dish");
+        typeList.add("dish");
+        typeList.add("drink");
+//        item.setType_menuitems(typeList);
+         Menu item =new Menu(typeList,menuList,priceList);
+//         System.out.println( item.getType_menuitems());
+        // item.displayMenu();
+         item.changeprice("meat", 500);
+        //  item.displayMenu();
+          item.removeitem("fish");
+        //  item.displayMenu();
+          item.additem("dish","chicken",900);
+       //  item.displayMenu();
+          item.additem("dish", "pizza",150);
+          item.additem("drink", "tea", 50);
+    item.displayMenu();
+          
+    
+    
+    System.out.println("end");
+   /////////////////////////////////////////////////////////////
+   /////////////////////////////////////////////////////////////
+         
     }
     
 }
