774d9e436bec9142052b5a342cbd6d028e992905
AlaaAtef2003 <alaaatef413@gmail.com>
2024-04-13 14:43:48 +0200

    update Tables,stuff and receptionist class

--- projectpart1/src/projectpart1/Stuff.java
+++ projectpart1/src/projectpart1/Stuff.java
@@ -8,6 +8,61 @@ package projectpart1;
  *
  * @author dell
  */
-public class Stuff {
+public abstract class Stuff {
+   private int salary ;
+   private int ID=1;
+   private static int count=1;
+   private String roles;
+   private int dayoff;
+
+    public Stuff() {
+        ID=count;
+        count++;
+      
+    }
+
+    public Stuff(int salary, String roles, int dayoff) {
+        this.salary = salary;
+        this.roles = roles;
+        this.dayoff = dayoff;
+       ID=count;
+        count++;
+    }
+    
+    
+    //getter and setter
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public int getDayoff() {
+        return dayoff;
+    }
+
+    public void setDayoff(int dayoff) {
+        this.dayoff = dayoff;
+    }
+     
+    
     
 }
