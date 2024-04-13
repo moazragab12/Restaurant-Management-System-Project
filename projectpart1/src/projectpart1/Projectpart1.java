2af0dc55c886d587be1edae139f72c03f2dccd52
EsraaAtef2101016 <esraaatef011529@gmail.com>
2024-04-14 00:38:20 +0200

    Remove Comments and add method getprice

--- projectpart1/src/projectpart1/Projectpart1.java
+++ projectpart1/src/projectpart1/Projectpart1.java
@@ -29,58 +29,29 @@ public class Projectpart1 {
          
          
          System.out.println("\n"+"Esraa"+"\n");
-         
-         //     Menu item =new Menu();
-      
-//      item.additem("fish");
-//      item.additem("meat");
-//      item.additem("chicken");
-//      
-//      System.out.println( item.getMenuitems());
-      
-//      item.removeitem("meat");
-//      System.out.println("After removing 'meat': " +item.getMenuitems()); 
-//        
-       ArrayList<String> menuList = new ArrayList<>();
+   
+        ArrayList<String> menuList = new ArrayList<>();
         menuList.add("fish");
         menuList.add("meat");
         menuList.add("coffee");
-//        item.setMenuitems(menuList);
-//         System.out.println( item.getMenuitems());
-         
-         
-         ArrayList<Double> priceList = new ArrayList<>();
+ 
+        ArrayList<Double> priceList = new ArrayList<>();
         priceList.add(200.0);
         priceList.add(300.0);
         priceList.add(100.0);
 
- 
-//        item.setPrice_menuitems(priceList);
-//         System.out.println( item.getPrice_menuitems());
-        //changeprice("fish",200000);
-        // item.displayMenu();
-         
-         ArrayList<String> typeList = new ArrayList<>();
+        ArrayList<String> typeList = new ArrayList<>();
         typeList.add("dish");
         typeList.add("dish");
         typeList.add("drink");
-//        item.setType_menuitems(typeList);
-         Menu item =new Menu(typeList,menuList,priceList);
-//         System.out.println( item.getType_menuitems());
-        // item.displayMenu();
-         item.changeprice("meat", 500);
-        //  item.displayMenu();
-          item.removeitem("fish");
-        //  item.displayMenu();
-          item.additem("dish","chicken",900);
-       //  item.displayMenu();
-          item.additem("dish", "pizza",150);
-          item.additem("drink", "tea", 50);
-    item.displayMenu();
-          
-    
-    
-    System.out.println("end");
+
+        Menu item =new Menu(typeList,menuList,priceList);
+
+        item.additem("dish","chicken",900);
+        item.additem("dish", "pizza",150);
+        item.additem("drink", "tea", 50);
+        item.displayMenu();
+        System.out.println("end");
    /////////////////////////////////////////////////////////////
    /////////////////////////////////////////////////////////////
          
