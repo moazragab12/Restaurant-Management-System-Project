68d1ea9f7c1e6b515a12d7b2ae7e09ebf98954e1
Moaz Ragab <moazr8365@gmail.com>
2024-05-10 01:37:57 +0300

    Changed some names of variables to Camel Style

--- projectpart1/src/main/java/org/example/projectpart1/Menu.java
+++ projectpart1/src/main/java/org/example/projectpart1/Menu.java
@@ -7,14 +7,14 @@ import java.util.Collections;
 public class Menu extends Items {
     protected static ArrayList<String>menuItems = new ArrayList<>();
     protected static ArrayList<Double>priceMenuItems=new ArrayList<>();
-    protected static ArrayList<String>type_menuItems = new ArrayList<>();
+    protected static ArrayList<String>typeMenuItems = new ArrayList<>();
 
     public Menu() {}
 
-    public Menu(ArrayList<String> type_menuItems,ArrayList<String> menuItems,ArrayList<Double> priceMenuItems){
+    public Menu(ArrayList<String> typeMenuItems,ArrayList<String> menuItems,ArrayList<Double> priceMenuItems){
         this.setmenuItems( menuItems);
         this.setprice_menuItems( priceMenuItems);
-        this.setType_menuItems(type_menuItems);
+        this.setType_menuItems(typeMenuItems);
     }
 
     // setter and getter menuItems
@@ -37,21 +37,21 @@ public class Menu extends Items {
 
     //    setter and getter type_menuItems
     public ArrayList<String> getType_menuItems() {
-        return type_menuItems;
+        return typeMenuItems;
     }
-    public void setType_menuItems(ArrayList<String> type_menuItems) {
-        this.type_menuItems = type_menuItems;
+    public void setType_menuItems(ArrayList<String> typeMenuItems) {
+        this.typeMenuItems = typeMenuItems;
     }
 
 
     @Override
     public void additem(String type,String item,double price) {
-        int lastIndex = type_menuItems.lastIndexOf(type);
+        int lastIndex = typeMenuItems.lastIndexOf(type);
 
         int m=0;
         if (lastIndex != -1) {
-            for (int i = type_menuItems.indexOf(type); i <= lastIndex; i++) {
-                if (type.equals(type_menuItems.get(i)) && item.equals(menuItems.get(i))) {
+            for (int i = typeMenuItems.indexOf(type); i <= lastIndex; i++) {
+                if (type.equals(typeMenuItems.get(i)) && item.equals(menuItems.get(i))) {
                     if(priceMenuItems.get(i)==price){
                         System.out.println("Item already exists in the menu.");
                         m=1;
@@ -63,7 +63,7 @@ public class Menu extends Items {
             if(lastIndex == -1) {lastIndex= menuItems.size()-1;}
             menuItems.add(lastIndex+1,item);
             priceMenuItems.add(lastIndex+1,price);
-            type_menuItems.add(lastIndex+1,type);
+            typeMenuItems.add(lastIndex+1,type);
         }
     }
 
@@ -72,18 +72,18 @@ public class Menu extends Items {
         int i=menuItems.indexOf(item);
         menuItems.remove(item);
         priceMenuItems.remove(i);
-        type_menuItems.remove(i);
+        typeMenuItems.remove(i);
 
     }
 
 
     public void displayMenu(){
         System.out.println("Menu");
-        System.out.println(type_menuItems.get(0));
+        System.out.println(typeMenuItems.get(0));
         System.out.println("\t"+menuItems.get(0)+"\t"+ priceMenuItems.get(0));
         for(int i=1 ; i< menuItems.size() ; i++  ){
-            if ( !((type_menuItems.get(i)).equals(type_menuItems.get(i-1))) )
-                System.out.println(type_menuItems.get(i));
+            if ( !((typeMenuItems.get(i)).equals(typeMenuItems.get(i-1))) )
+                System.out.println(typeMenuItems.get(i));
             System.out.println("\t"+menuItems.get(i)+"\t"+ priceMenuItems.get(i));
 
 
@@ -134,11 +134,11 @@ public class Menu extends Items {
         Menu x = new Menu();
         x.menuItems.addAll(o.menuItems);
         x.priceMenuItems.addAll(o.priceMenuItems);
-        x.type_menuItems.addAll(o.type_menuItems);
+        x.typeMenuItems.addAll(o.typeMenuItems);
         Collections.sort(o.priceMenuItems);
         for(int i=0;i<o.priceMenuItems.size();i++){
             int index= x.priceMenuItems.indexOf(o.priceMenuItems.get(i));
-            System.out.println(type_menuItems.get(index)+"\t"+menuItems.get(index)+"\t"+priceMenuItems.get(i));
+            System.out.println(typeMenuItems.get(index)+"\t"+menuItems.get(index)+"\t"+priceMenuItems.get(i));
         }
     }
 
