da54445a2c6def085935a633c5b21c6366185dd7
Moaz Ragab <moazr8365@gmail.com>
2024-05-10 14:13:51 +0300

    Edit Gui

--- projectpart1/src/main/java/org/example/projectpart1/MainController.java
+++ projectpart1/src/main/java/org/example/projectpart1/MainController.java
@@ -151,6 +151,10 @@ TextField removalText;
     private Parent root;
     public void ChangeManager(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Manager.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.close();
 
     }
 }
